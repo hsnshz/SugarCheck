db7a40f5f1a769f9c5f800198af572fba0a50009
hsnshz <hsn.shz63@gmail.com>
2023-12-25 19:08:57 +0300

    Updated Diabetes Prediction Form UI and added navigation

---
+++ Frontend/SugarCheck/src/components/FormItem.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const FormItem = ({ label, onValueChange, selectedValue, options }) => {
+  return (
+    <View style={styles.formItem}>
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.radioGroup}>
+        {options.map((option) => (
+          <View key={option.value} style={styles.radioOptionContainer}>
+            <TouchableOpacity
+              style={styles.radio}
+              onPress={() => onValueChange(option.value)}
+            >
+              <View style={styles.outerCircle}>
+                {selectedValue === option.value && (
+                  <View style={styles.innerCircle} />
+                )}
+              </View>
+            </TouchableOpacity>
+            <Text style={styles.radioText}>{option.label}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginBottom: 20,
+  },
+  label: {
+    width: 110,
+    fontFamily: "MontserratRegular",
+    fontSize: 18,
+    marginRight: 10,
+    marginLeft: 48,
+  },
+  radioGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  radioOptionContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    minWidth: 70,
+  },
+  outerCircle: {
+    height: 24,
+    width: 24,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 5,
+  },
+  innerCircle: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: "black",
+  },
+  radioText: {
+    fontFamily: "MontserratRegular",
+    fontSize: 16,
+  },
+});
+
+export default FormItem;
--- Frontend/SugarCheck/src/screens/DiabetesFormScreen.js
+++ Frontend/SugarCheck/src/screens/DiabetesFormScreen.js
@@ -8,88 +8,222 @@ import {
   ScrollView,
   SafeAreaView,
 } from "react-native";
-
-const FormItem = ({ label, onValueChange, selectedValue, options }) => {
-  return (
-    <SafeAreaView style={styles.formItem}>
-      <Text style={styles.label}>{label}</Text>
-      <View style={styles.radioGroup}>
-        {options.map((option) => (
-          <TouchableOpacity
-            key={option.value}
-            style={styles.radio}
-            onPress={() => onValueChange(option.value)}
-          >
-            <View style={styles.outerCircle}>
-              {selectedValue === option.value && (
-                <View style={styles.innerCircle} />
-              )}
-            </View>
-            <Text style={styles.radioText}>{option.label}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </SafeAreaView>
-  );
-};
+import FormItem from "../components/FormItem";
+import colors from "../../config/colors";
+import { ButtonSecondary, Heading, Subheading } from "../../config/styledText";
 
 const DiabetesForm = () => {
   const [formData, setFormData] = useState({
     age: "",
     gender: "",
     polyuria: "",
-    // ...other fields
+    polydipsia: "",
+    suddenWeightLoss: "",
+    weakness: "",
+    polyphagia: "",
+    genitalThrush: "",
+    visualBlurring: "",
+    itching: "",
+    irritability: "",
+    delayedHealing: "",
+    partialParesis: "",
+    muscleStiffness: "",
+    alopecia: "",
+    obesity: "",
   });
 
   const handleRadioChange = (name, value) => {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleSubmit = () => {
+    console.log(formData);
+  };
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Diabetes Prediction</Text>
-        <Text style={styles.subheaderText}>
-          Please Enter To Your Best Of Knowledge To Receive A Prediction
-        </Text>
-      </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView>
+        <View style={styles.header}>
+          <Heading style={styles.headerText}>Diabetes Prediction</Heading>
+          <Subheading style={styles.subheaderText}>
+            Please Enter To Your Best Of Knowledge To Receive A Prediction
+          </Subheading>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={formData.age}
+            onChangeText={(text) => setFormData({ ...formData, age: text })}
+            placeholder="Enter Age"
+            keyboardType="numeric"
+          />
+        </View>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Age</Text>
-        <TextInput
-          style={styles.input}
-          value={formData.age}
-          onChangeText={(text) => setFormData({ ...formData, age: text })}
-          placeholder="Enter Age"
-          keyboardType="numeric"
+        <FormItem
+          label="Gender"
+          selectedValue={formData.gender}
+          onValueChange={(value) => handleRadioChange("gender", value)}
+          options={[
+            { label: "Male", value: "male" },
+            { label: "Female", value: "female" },
+          ]}
         />
-      </View>
 
-      {/* Use FormItem for Yes/No questions */}
-      <FormItem
-        label="Polyuria"
-        selectedValue={formData.polyuria}
-        onValueChange={(value) => handleRadioChange("polyuria", value)}
-        options={[
-          { label: "Yes", value: "yes" },
-          { label: "No", value: "no" },
-        ]}
-      />
+        <FormItem
+          label="Polyuria"
+          selectedValue={formData.polyuria}
+          onValueChange={(value) => handleRadioChange("polyuria", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
 
-      {/* Repeat FormItem for each yes/no question */}
+        <FormItem
+          label="Polydipsia"
+          selectedValue={formData.polydipsia}
+          onValueChange={(value) => handleRadioChange("polydipsia", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
 
-      {/* Submit Button */}
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Submit</Text>
-      </TouchableOpacity>
-    </ScrollView>
+        <FormItem
+          label="Sudden Weight Loss"
+          selectedValue={formData.suddenWeightLoss}
+          onValueChange={(value) =>
+            handleRadioChange("suddenWeightLoss", value)
+          }
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Weakness"
+          selectedValue={formData.weakness}
+          onValueChange={(value) => handleRadioChange("weakness", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Polyphagia"
+          selectedValue={formData.polyphagia}
+          onValueChange={(value) => handleRadioChange("polyphagia", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Genital Thrush"
+          selectedValue={formData.genitalThrush}
+          onValueChange={(value) => handleRadioChange("genitalThrush", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Visual Blurring"
+          selectedValue={formData.visualBlurring}
+          onValueChange={(value) => handleRadioChange("visualBlurring", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Itching"
+          selectedValue={formData.itching}
+          onValueChange={(value) => handleRadioChange("itching", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Irritability"
+          selectedValue={formData.irritability}
+          onValueChange={(value) => handleRadioChange("irritability", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Delayed Healing"
+          selectedValue={formData.delayedHealing}
+          onValueChange={(value) => handleRadioChange("delayedHealing", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Partial Paresis"
+          selectedValue={formData.partialParesis}
+          onValueChange={(value) => handleRadioChange("partialParesis", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Muscle Stiffness"
+          selectedValue={formData.muscleStiffness}
+          onValueChange={(value) => handleRadioChange("muscleStiffness", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Alopecia"
+          selectedValue={formData.alopecia}
+          onValueChange={(value) => handleRadioChange("alopecia", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+
+        <FormItem
+          label="Obesity"
+          selectedValue={formData.obesity}
+          onValueChange={(value) => handleRadioChange("obesity", value)}
+          options={[
+            { label: "Yes", value: "yes" },
+            { label: "No", value: "no" },
+          ]}
+        />
+        <View style={styles.centerButtonView}>
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <ButtonSecondary>Submit</ButtonSecondary>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: "#f2f2f2",
+    backgroundColor: colors.background,
   },
   header: {
     padding: 20,
@@ -110,60 +244,26 @@ const styles = StyleSheet.create({
   inputContainer: {
     padding: 20,
   },
-  label: {
-    fontSize: 16,
-    color: "#000",
-    marginBottom: 10,
-  },
   input: {
-    fontSize: 16,
-    backgroundColor: "#fff",
-    padding: 10,
-    borderRadius: 4,
-    marginBottom: 20,
-  },
-  formItem: {
-    padding: 20,
-  },
-  radioGroup: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  radio: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginRight: 20,
-  },
-  outerCircle: {
-    height: 24,
-    width: 24,
-    borderRadius: 12,
+    fontFamily: "MontserratRegular",
+    width: "85%",
+    height: 40,
+    borderColor: "gray",
     borderWidth: 1,
-    borderColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  innerCircle: {
-    height: 12,
-    width: 12,
-    borderRadius: 6,
-    backgroundColor: "#000",
-  },
-  radioText: {
-    fontSize: 16,
-    marginLeft: 8,
+    marginBottom: 10,
+    padding: 10,
+    alignSelf: "center",
   },
   button: {
-    backgroundColor: "#000",
-    padding: 15,
-    borderRadius: 4,
+    backgroundColor: colors.complementary,
+    padding: 10,
+    borderRadius: 5,
+    width: "40%",
     alignItems: "center",
     margin: 20,
   },
-  buttonText: {
-    fontSize: 16,
-    color: "#fff",
-    fontWeight: "600",
+  centerButtonView: {
+    alignItems: "center",
   },
 });
 
