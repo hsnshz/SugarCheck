5f5fc7dfc7cb95270c0017bd668a65543804f922
hsnshz <hsn.shz63@gmail.com>
2023-12-28 01:50:25 +0300

    Styled UI for diabetes form screen and made some minor adjustments to FormItem styling and removed successful sign in alert

--- Frontend/SugarCheck/src/screens/DiabetesFormScreen.js
+++ Frontend/SugarCheck/src/screens/DiabetesFormScreen.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   View,
   Text,
+  SafeAreaView,
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  ScrollView,
-  SafeAreaView,
+  Dimensions,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import FormItem from "../components/FormItem";
 import colors from "../../config/colors";
 import { ButtonSecondary, Heading, Subheading } from "../../config/styledText";
+import Carousel from "react-native-reanimated-carousel";
+
+const { width: viewportWidth } = Dimensions.get("window");
 
 const DiabetesForm = () => {
   const [formData, setFormData] = useState({
@@ -31,6 +37,134 @@ const DiabetesForm = () => {
     alopecia: "",
     obesity: "",
   });
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselRef = useRef(null);
+
+  const formQuestions = [
+    { field: "age", label: "Age", type: "input", keyboardType: "numeric" },
+    {
+      field: "gender",
+      label: "Gender",
+      type: "radio",
+      options: ["Male", "Female"],
+    },
+    {
+      field: "polyuria",
+      label: "Polyuria",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "polydipsia",
+      label: "Polydipsia",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "suddenWeightLoss",
+      label: "Sudden Weight Loss",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "weakness",
+      label: "Weakness",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "polyphagia",
+      label: "Polyphagia",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "genitalThrush",
+      label: "Genital Thrush",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "visualBlurring",
+      label: "Visual Blurring",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "itching",
+      label: "Itching",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "irritability",
+      label: "Irritability",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "delayedHealing",
+      label: "Delayed Healing",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "partialParesis",
+      label: "Partial Paresis",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "muscleStiffness",
+      label: "Muscle Stiffness",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "alopecia",
+      label: "Alopecia",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+    {
+      field: "obesity",
+      label: "Obesity",
+      type: "radio",
+      options: ["Yes", "No"],
+    },
+  ];
+
+  const formSteps = [
+    {
+      key: "step1",
+      questions: formQuestions.slice(0, 3),
+    },
+    {
+      key: "step2",
+      questions: formQuestions.slice(3, 6),
+    },
+    {
+      key: "step3",
+      questions: formQuestions.slice(6, 9),
+    },
+    {
+      key: "step4",
+      questions: formQuestions.slice(9, 12),
+    },
+    {
+      key: "step5",
+      questions: formQuestions.slice(12, 16),
+    },
+  ];
+
+  const isStepComplete = (stepIndex) => {
+    const stepQuestions = formSteps[stepIndex].questions;
+
+    return stepQuestions.every((question) => {
+      const value = formData[question.field];
+      return value !== null && value !== "";
+    });
+  };
 
   const handleRadioChange = (name, value) => {
     setFormData({ ...formData, [name]: value });
@@ -40,219 +174,154 @@ const DiabetesForm = () => {
     console.log(formData);
   };
 
-  return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView>
-        <View style={styles.header}>
-          <Heading style={styles.headerText}>Diabetes Prediction</Heading>
-          <Subheading style={styles.subheaderText}>
-            Please Enter To Your Best Of Knowledge To Receive A Prediction
-          </Subheading>
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            value={formData.age}
-            onChangeText={(text) => setFormData({ ...formData, age: text })}
-            placeholder="Enter Age"
-            keyboardType="numeric"
+  const renderQuestion = (question) => {
+    switch (question.type) {
+      case "input":
+        return (
+          <View style={styles.inputRow}>
+            <Text style={styles.inputLabel}>{question.label}</Text>
+            <TextInput
+              style={styles.input}
+              value={formData[question.field]}
+              onChangeText={(text) =>
+                setFormData({ ...formData, [question.field]: text })
+              }
+              placeholder={question.label}
+              keyboardType={question.keyboardType}
+            />
+          </View>
+        );
+      case "radio":
+        return (
+          <FormItem
+            label={question.label}
+            selectedValue={formData[question.field]}
+            onValueChange={(value) => handleRadioChange(question.field, value)}
+            options={question.options.map((option) => ({
+              label: option,
+              value: option.toLowerCase(),
+            }))}
           />
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderItem = ({ item, index }) => {
+    const stepComplete = isStepComplete(index);
+    return (
+      <View style={styles.slide} key={`step-${item.key}`}>
+        <View style={styles.card}>
+          {item.questions.map((question, qIndex) => (
+            <React.Fragment key={`question-${qIndex}`}>
+              {renderQuestion(question)}
+            </React.Fragment>
+          ))}
         </View>
-
-        <FormItem
-          label="Gender"
-          selectedValue={formData.gender}
-          onValueChange={(value) => handleRadioChange("gender", value)}
-          options={[
-            { label: "Male", value: "male" },
-            { label: "Female", value: "female" },
-          ]}
-        />
-
-        <FormItem
-          label="Polyuria"
-          selectedValue={formData.polyuria}
-          onValueChange={(value) => handleRadioChange("polyuria", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Polydipsia"
-          selectedValue={formData.polydipsia}
-          onValueChange={(value) => handleRadioChange("polydipsia", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Sudden Weight Loss"
-          selectedValue={formData.suddenWeightLoss}
-          onValueChange={(value) =>
-            handleRadioChange("suddenWeightLoss", value)
-          }
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Weakness"
-          selectedValue={formData.weakness}
-          onValueChange={(value) => handleRadioChange("weakness", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Polyphagia"
-          selectedValue={formData.polyphagia}
-          onValueChange={(value) => handleRadioChange("polyphagia", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Genital Thrush"
-          selectedValue={formData.genitalThrush}
-          onValueChange={(value) => handleRadioChange("genitalThrush", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Visual Blurring"
-          selectedValue={formData.visualBlurring}
-          onValueChange={(value) => handleRadioChange("visualBlurring", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Itching"
-          selectedValue={formData.itching}
-          onValueChange={(value) => handleRadioChange("itching", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Irritability"
-          selectedValue={formData.irritability}
-          onValueChange={(value) => handleRadioChange("irritability", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Delayed Healing"
-          selectedValue={formData.delayedHealing}
-          onValueChange={(value) => handleRadioChange("delayedHealing", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Partial Paresis"
-          selectedValue={formData.partialParesis}
-          onValueChange={(value) => handleRadioChange("partialParesis", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Muscle Stiffness"
-          selectedValue={formData.muscleStiffness}
-          onValueChange={(value) => handleRadioChange("muscleStiffness", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Alopecia"
-          selectedValue={formData.alopecia}
-          onValueChange={(value) => handleRadioChange("alopecia", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
-
-        <FormItem
-          label="Obesity"
-          selectedValue={formData.obesity}
-          onValueChange={(value) => handleRadioChange("obesity", value)}
-          options={[
-            { label: "Yes", value: "yes" },
-            { label: "No", value: "no" },
-          ]}
-        />
         <View style={styles.centerButtonView}>
-          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-            <ButtonSecondary>Submit</ButtonSecondary>
+          <TouchableOpacity
+            style={[styles.button, !stepComplete && styles.buttonDisabled]}
+            onPress={() => {
+              if (index === formSteps.length - 1) {
+                handleSubmit();
+              } else {
+                carouselRef.current?.scrollTo({
+                  index: index + 1,
+                  animated: true,
+                });
+              }
+            }}
+            disabled={!stepComplete}
+          >
+            <ButtonSecondary>
+              {index === formSteps.length - 1 ? "Submit" : "Continue"}
+            </ButtonSecondary>
           </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+    );
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.header}>
+          <Heading>Diabetes Prediction</Heading>
+          <Subheading style={styles.subheaderText}>
+            Please enter to the best of your knowledge to receive a prediction
+          </Subheading>
+        </View>
+
+        <Carousel
+          mode="horizontal-stack"
+          modeConfig={{ snapDirection: "left" }}
+          ref={carouselRef}
+          data={formSteps}
+          renderItem={renderItem}
+          width={viewportWidth}
+          height={400}
+          loop={false}
+          lockScrollWhileSnapping
+          onSnapToItem={(index) => {
+            if (isStepComplete(currentIndex)) {
+              setCurrentIndex(index);
+            } else if (index > currentIndex) {
+              carouselRef.current?.scrollTo({
+                index: currentIndex,
+                animated: true,
+              });
+              Alert.alert("Please fill out all fields to continue");
+            }
+          }}
+        />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    flex: 1,
+    flex: 0.9,
     backgroundColor: colors.background,
+    justifyContent: "center",
   },
   header: {
-    padding: 20,
+    padding: 30,
     alignItems: "center",
     borderBottomWidth: 1,
     borderBottomColor: "#ddd",
   },
-  headerText: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
   subheaderText: {
-    fontSize: 16,
-    color: "#666",
     textAlign: "center",
-    marginTop: 8,
+    marginTop: 10,
   },
   inputContainer: {
     padding: 20,
   },
+  inputRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
+    marginBottom: 25,
+  },
+  inputLabel: {
+    // flex: 1,
+    width: 110,
+    fontFamily: "MontserratRegular",
+    fontSize: 18,
+    marginLeft: 10,
+    marginRight: 10,
+  },
   input: {
     fontFamily: "MontserratRegular",
-    width: "85%",
-    height: 40,
+    height: 35,
     borderColor: "gray",
     borderWidth: 1,
-    marginBottom: 10,
     padding: 10,
-    alignSelf: "center",
+    flex: 1,
+    // minWidth: 175,
   },
   button: {
     backgroundColor: colors.complementary,
@@ -264,6 +333,43 @@ const styles = StyleSheet.create({
   },
   centerButtonView: {
     alignItems: "center",
+  },
+  slide: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    backgroundColor: colors.active,
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "85%",
+    height: "65%",
+    alignSelf: "center",
+  },
+  questionContainer: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  centerButtonView: {
+    marginTop: 20,
+    width: "85%",
+  },
+  button: {
+    backgroundColor: colors.complementary,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonDisabled: {
+    backgroundColor: colors.inactive,
   },
 });
 
