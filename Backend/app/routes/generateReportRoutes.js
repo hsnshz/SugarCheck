b1029588216435fc8077750b829792efce625588
hsnshz <hsn.shz63@gmail.com>
2024-03-26 19:59:32 +0000

    Terms and conditions and new folder structure for flask

--- Backend/app/routes/generateReportRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-const router = Router();
-import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { generateReport } from "../controllers/ReportGenerationController.js";
-
-router.post("/generate/:id", authMiddleware, generateReport);
-
-export default router;
