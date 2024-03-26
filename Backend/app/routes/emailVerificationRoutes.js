8b0e10b2ad0ca1a0df1ac1193b15b858c540f9d4
hsnshz <hsn.shz63@gmail.com>
2024-03-26 19:59:32 +0000

    Terms and conditions and new folder structure for flask

--- Backend/app/routes/emailVerificationRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { verifyEmail } from "../controllers/emailVerificationController.js";
-
-router.post("/verify-email", verifyEmail);
-
-export default router;
