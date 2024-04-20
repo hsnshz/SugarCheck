3791241101a06aa27e21db03733e8efe85ded692
hsnshz <hsn.shz63@gmail.com>
2024-04-20 22:15:43 +0100

    puppeteer heroku bug fix v2

--- Backend/app/controllers/ReportGenerationController.js
+++ Backend/app/controllers/ReportGenerationController.js
@@ -123,7 +123,6 @@ export async function generateReport(req, res) {
 
     // Generate PDF
     const browser = await puppeteer.launch({
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
@@ -131,6 +130,9 @@ export async function generateReport(req, res) {
         "--disable-gpu",
       ],
       headless: true,
+      executablePath:
+        process.env.PUPPETEER_EXECUTABLE_PATH ||
+        "/app/.apt/usr/bin/google-chrome",
     });
 
     const page = await browser.newPage();
