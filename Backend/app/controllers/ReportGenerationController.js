c642e32ccceae36c90299f13bbfc96b2cce93a64
hsnshz <hsn.shz63@gmail.com>
2024-04-20 21:57:20 +0100

    Puppeteer bug fix for heroku

--- Backend/app/controllers/ReportGenerationController.js
+++ Backend/app/controllers/ReportGenerationController.js
@@ -122,7 +122,17 @@ export async function generateReport(req, res) {
     await report.save();
 
     // Generate PDF
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--headless",
+        "--disable-gpu",
+      ],
+      headless: true,
+    });
+
     const page = await browser.newPage();
 
     // Define HTML template
